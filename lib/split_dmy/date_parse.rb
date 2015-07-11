ade74a99ba8f1654495622dd67a053b18c024126
Colin Bruce <colinbruce@gmail.com>
2015-07-11 22:37:51 +0100

    implement changes for rubocop

--- lib/split_dmy/date_parse.rb
+++ lib/split_dmy/date_parse.rb
@@ -3,13 +3,13 @@ module SplitDmy
 
     def validate_date(field)
       date = Date.new(
-          instance_variable_get("@#{field}_year"),
-          instance_variable_get("@#{field}_month"),
-          instance_variable_get("@#{field}_day")
+        instance_variable_get("@#{field}_year"),
+        instance_variable_get("@#{field}_month"),
+        instance_variable_get("@#{field}_day")
       )
-      self.send("#{field}=", date)
+      send("#{field}=", date)
     rescue
-      self.send("#{field}=", nil)
+      send("#{field}=", nil)
     end
   end
 end
